78ba5f2889d68b3a4d36e5b472c26bda8473a18e
Gibran Castillo <gibran-castillo@hotmail.com>
2022-01-10 12:54:57 +0300

    Init commit

---
+++ tictactoe.py
@@ -0,0 +1,24 @@
+'''
+Author: Brother Castillo
+File: tictactoe.py
+Purpose: Tic-Tac-Toe is a game in which two players seek in alternate turns to complete a row, a column, 
+         or a diagonal with either three x's or three o's drawn in the spaces of a grid of nine squares.
+'''
+
+
+def main():
+    '''
+    Pending
+    '''
+    init_game()
+
+
+def init_game():
+    print("1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9")
+
+# If this file was executed like this: % python3 tictactoe.py
+# then call the main function. However, if this file
+# was simply imported, then skip the call to main.
+if __name__ == "__main__":
+    # Start this program by calling the main function.
+    main()
